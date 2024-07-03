2c07d8233208c4a5f69a31bf92248fc55c9e424b
Yusri Amir <yusriamir0@gmail.com>
2024-07-03 20:54:18 +0800

    add enum for transaction and acount

--- be-server/model/Account.js
+++ be-server/model/Account.js
@@ -1,16 +1,41 @@
 import { pool } from "../config/connection.js";
 
 const query = `
-C
+
+CREATE TYPE account_type AS ENUM(
+  'Savings',
+  'Invesment',
+  'Checking',
+  'Credit card',
+  'Building',
+  'School',
+  'Project',
+  'Utilities',
+  'Travel',
+  'Personal',
+  'Loan',
+  'Organization',
+  'Grocery',
+  'Food',
+  'Entertainment',
+  'Other'
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+  name VARCHAR(255) UNIQUE NOT NULL,
+  account_type account_type NOT NULL,
+  initial_balance NUMERIC(7,2) DEFAULT 0,
+  id serial PRIMARY KEY, -- will be pushed to Transactions model
+  user_id INTEGER REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 `;
 
 const createAccountTable = async () => {
   try {
     await pool.query(query);
-    console.log("User table created");
-  } catch (error) {
-    console.log(error);
-  }
+    console.log("User account created");
+  } catch (error) {}
 };
 
 export default createAccountTable;
--- be-server/model/Transaction.js
+++ be-server/model/Transaction.js
@@ -1,23 +1,43 @@
 import { pool } from "../config/connection.js";
 
 const query = `
+CREATE TYPE transaction_type AS ENUM(
+  'expense',
+  'income'
+);
+
+CREATE TYPE category AS ENUM(
+'Food',
+'Transportation',
+'Entertainment',
+'Shopping',
+'Utilities',
+'Healthcare',
+'Travel',
+'Education',
+'Personal',
+'Groceries',
+'Bills',
+'Uncategorized'
+);
+
 CREATE TABLE IF NOT EXISTS transactions(
-    id SERIAL PRIMARY KEY,
-    text VARCHAR(255) NOT NULL,
-    status BOOLEAN DEFAULT FALSE,
-    amount INT NOT NULL,
-    user_id INTEGER REFERENCES users(id),
-    created_at TIMESTAMP DEFAULT NOW() 
+  name VARCHAR(255) NOT NULL,
+  transaction_type transaction_type NOT NULL,
+  category category NOT NULL,
+  amount INT NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  date DATE NOT NULL DEFAULT CURRENT_DATE,
+  created_at TIMESTAMP DEFAULT NOW(),
+  id SERIAL PRIMARY KEY
 )
 `;
 
-const createTransaction = async () => {
+const createTransactionTable = async () => {
   try {
     await pool.query(query);
     console.log("Transaction created");
-  } catch (error) {
-    console.error(error);
-  }
+  } catch (error) {}
 };
 
-export default createTransaction;
+export default createTransactionTable;
